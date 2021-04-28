b247697100cbc168a30d284bb17894edda871f89
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 15:58:44 +0300

    Second commit

---
+++ ChessBoard/ChessBoard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoard
+{
+    class ChessBoard
+    {
+        private uint height;
+        private uint width;
+
+        public ChessBoard() { }
+        public ChessBoard(uint height, uint width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public void printChessBoard()
+        {
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write("* ");
+                    else
+                        Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -6,7 +6,22 @@ namespace ChessBoard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint m, n;
+            ChessBoard chessBoard;
+
+            Console.WriteLine("Введите высоту шахматной доски: ");
+            while (!uint.TryParse(Console.ReadLine(), out m))
+                Console.WriteLine("Введите целое положительное число!: ");
+
+            Console.WriteLine("Введите ширину шахматной доски: ");
+            while (!uint.TryParse(Console.ReadLine(), out n))
+                Console.WriteLine("Введите целое положительное число!: ");
+
+            chessBoard = new ChessBoard(m, n);
+            chessBoard.printChessBoard();
+
+            Console.WriteLine("Программа завершена");
+            Console.ReadKey();
         }
     }
 }
