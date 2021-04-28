cd01791dbfaa08be744ad38fec2feadcf9fe2f7b
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 15:58:10 +0300

    First commit

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
