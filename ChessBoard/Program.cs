c36cd182929d4fd36b0417ba16c0aebe8af581e2
Вячеслав Островский <slslava007@gmail.com>
2021-05-26 11:16:53 +0300

    Added arguments

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -24,12 +24,50 @@ namespace ChessBoard
 
             return w;
         }
-        static void PrintInstructions()
+        static void PrintTask()
         {
-
+            Console.WriteLine("Вывести шахматную доску с заданными размерами высоты и ширины, по принципу: " +
+                "\n* * * * * * " +
+                "\n * * * * * * " +
+                "\n* * * * * * " +
+                "\n * * * * * * " +
+                "\nПрограмма запускается через вызов главного класса с параметрами.");
+        }
+        static void PrintHelp()
+        {
+            Console.WriteLine("Возможные параметры:" +
+                "\n-help - выводит информация о парамтрах" +
+                "\n-task - выводит условие задания");
+        }
+        static void PrintInstructions(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                PrintHelp();
+            }
+            else
+            {
+                foreach(string str in args)
+                {
+                    switch (str)
+                    {
+                        case "-help":
+                            PrintHelp();
+                            break;
+                        case "-task":
+                            PrintTask();
+                            break;
+                        default:
+                            Console.WriteLine("Данный агрумент не правильный");
+                            break;
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
+            PrintInstructions(args);
+
             ChessBoard chessBoard = new ChessBoard(GetHeight(), GetWidth());
             chessBoard.printChessBoard();
 
